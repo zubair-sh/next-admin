fb0cad93e9c161ba95032ec77e6a5d2c6067c0c9
Zubair <zubair.work3@gmail.com>
2026-01-16 16:49:14 +0500

    feat: implement Prisma PostgreSQL adapter and update database connection configuration.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,21 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  });
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
 };
 
-declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
-}
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
